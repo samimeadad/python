b8e8cad94f4570b0cac30c805c659604e695b89b
samimeadad <sami.meadad@gmail.com>
2021-02-12 21:07:58 +0600

    multiplication file added

---
+++ mul.py
@@ -0,0 +1,10 @@
+# This program adds two numbers
+
+num1 = 1.5
+num2 = 6.3
+
+# Add two numbers
+mul = num1*num2
+
+# Display the multiplication
+print('The multiplication of {0} and {1} is {2}'.format(num1, num2, mul))
