5c388746dab53ecc35ced5f4a95197a00453a572
samimeadad <sami.meadad@gmail.com>
2023-11-10 09:11:49 +0600

    python version check file added

---
+++ check-version.py
@@ -0,0 +1,7 @@
+import sys
+
+
+def print_python_version():
+    print(sys.version)
+
+print_python_version()
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+dataPath = "/Users/sami/Downloads/book1.xlsx";
+
+df = pd.read_excel(dataPath);
+
+print(df);
