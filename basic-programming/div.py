ac9bbafbdf1ddceff17ec8287e9c441e342e737f
samimeadad <sami.meadad@gmail.com>
2023-11-10 12:13:44 +0600

    format document added

--- basic-programming/div.py
+++ basic-programming/div.py
@@ -1,13 +1,13 @@
 # This program adds two numbers
 
-num1 = 11;
-num2 = 2;
+num1 = 11
+num2 = 2
 
-print(type(num1));
+print(type(num1))
 
 # Add two numbers
-div = num1/num2;
+div = num1/num2
 
 # Display the sum
-print('The division of {0} and {1} is {2}'.format(num1, num2, div));
-print(type(div));
+print('The division of {0} and {1} is {2}'.format(num1, num2, div))
+print(type(div))
